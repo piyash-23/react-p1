69c798763d47893d8cfbde9e095c5d6fd0af985c
piyash-23 <arafat.ahmed2004@gmail.com>
2025-09-18 23:48:49 +0600

    react on git

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Countries from "./components/countries/countries";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>React On the Go</h1>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/countries/countries.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Countries = () => {
+  return (
+    <div>
+      <h1>Bangladesh theke hi</h1>
+    </div>
+  );
+};
+
+export default Countries;
